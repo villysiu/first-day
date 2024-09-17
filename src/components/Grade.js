66a48a0a031b6bd0b761afce4eecfddcfac6caf7
Villy Siu <villysiu@gmail.com>
2024-09-17 13:43:19 -0700

    restore grade if click outside

--- src/components/Grade.js
+++ src/components/Grade.js
@@ -1,10 +1,12 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 const Grade = () =>{
     const [inputbox, setInputbox ]  = useState(false)
     const [grade, setGrade] = useState("Kindergarten")
 
     const grades = ["Kindergarten", "First Grade", "Second Grade", "Third Grade", "Fourth Grade", "Fifth Grade"]
+    const ref = useRef();
+
     const handleClick = () =>{
         setInputbox(!inputbox)
     }
@@ -13,10 +15,22 @@ const Grade = () =>{
         setGrade( selected_grade === "k" ?  grades[0] : grades[selected_grade] )
         setInputbox(!inputbox)
     }
+    useEffect(()=>{
+        const clickOutside = e =>{
+            
+            if(ref.current && !ref.current.contains(e.target)) {
+                setInputbox(false)
+            }
+        }
+        document.addEventListener("mousedown", clickOutside);
+        return () => {
+            document.removeEventListener("mousedown", clickOutside);
+        };
+    }, [])
     const GradeDropdown = (e) => {
         
         return (
-            <select name="grades" className="grade-dropdown" onChange={handleChange}>
+            <select ref={ref} name="grades" className="grade-dropdown" onChange={handleChange}>
                 <option className="opy" key="x">Select your grade</option>
                 {
                     grades.map((grade, idx)=>{
